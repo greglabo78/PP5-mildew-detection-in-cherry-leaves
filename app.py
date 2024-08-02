1da4c9a244498b2f36baf4479d5a5c64446becf1
greglabo78 <gregoryobigodwin@yahoo.com>
2024-08-02 10:13:41 +0000

    powdery mildew detector page & analysis

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from app_pages.multipage import MultiPage
 #load pages scripts
 from app_pages.page_project_summary import page_project_summary_body
 from app_pages.page_project_hypothesis import page_project_hypothesis_body
+from app_pages.page_powdery_mildew_detector import page_powdery_mildew_detector_body
 
 
 
@@ -13,6 +14,7 @@ app = MultiPage(app_name="Powdery Mildew Detector")  # Create an instance of the
 # Add your app pages here using .add_page()
 app.add_page("Project Summary", page_project_summary_body)
 app.add_page("Project Hypothesis", page_project_hypothesis_body)
+app.add_page("Powdery_Mildew Detection", page_powdery_mildew_detector_body)
 
 
 
