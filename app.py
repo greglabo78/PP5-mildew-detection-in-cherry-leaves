10a57027e17cc1ed753a6e3af7e38dc28fc03209
greglabo78 <gregoryobigodwin@yahoo.com>
2024-08-04 09:30:31 +0000

    deployment to go-live

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from app_pages.multipage import MultiPage
 from app_pages.page_project_summary import page_project_summary_body
 from app_pages.page_project_hypothesis import page_project_hypothesis_body
 from app_pages.page_powdery_mildew_detector import page_powdery_mildew_detector_body
+from app_pages.page_cherry_leaves_visualizer import page_cherry_leaves_visualizer_body
+from app_pages.page_ml_performance import page_ml_performance_metrics
 
 
 
@@ -15,6 +17,8 @@ app = MultiPage(app_name="Powdery Mildew Detector")  # Create an instance of the
 app.add_page("Project Summary", page_project_summary_body)
 app.add_page("Project Hypothesis", page_project_hypothesis_body)
 app.add_page("Powdery_Mildew Detection", page_powdery_mildew_detector_body)
+app.add_page("Cherry Leaves Visualiser", page_cherry_leaves_visualizer_body)
+app.add_page("ML Performance Metrics", page_ml_performance_metrics)
 
 
 
